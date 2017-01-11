a7741c8a776e5574092cb5512e3bce4e5d0621a6
Michael Svit <michaelsvit@gmail.com>
2017-01-11 10:36:37 +0200

    Base skeleton file

---
+++ src/FibonacciHeap.java
@@ -0,0 +1,139 @@
+/**
+ * FibonacciHeap
+ * <p>
+ * An implementation of fibonacci heap over non-negative integers.
+ */
+public class FibonacciHeap {
+
+    /**
+     * public boolean empty()
+     * <p>
+     * precondition: none
+     * <p>
+     * The method returns true if and only if the heap
+     * is empty.
+     */
+    public boolean empty() {
+        return false; // should be replaced by student code
+    }
+
+    /**
+     * public HeapNode insert(int key)
+     * <p>
+     * Creates a node (of type HeapNode) which contains the given key, and inserts it into the heap.
+     */
+    public HeapNode insert(int key) {
+        return new HeapNode(5); // should be replaced by student code
+    }
+
+    /**
+     * public void deleteMin()
+     * <p>
+     * Delete the node containing the minimum key.
+     */
+    public void deleteMin() {
+        return; // should be replaced by student code
+
+    }
+
+    /**
+     * public HeapNode findMin()
+     * <p>
+     * Return the node of the heap whose key is minimal.
+     */
+    public HeapNode findMin() {
+        return new HeapNode(5);// should be replaced by student code
+    }
+
+    /**
+     * public void meld (FibonacciHeap heap2)
+     * <p>
+     * Meld the heap with heap2
+     */
+    public void meld(FibonacciHeap heap2) {
+        return; // should be replaced by student code
+    }
+
+    /**
+     * public int size()
+     * <p>
+     * Return the number of elements in the heap
+     */
+    public int size() {
+        return 0; // should be replaced by student code
+    }
+
+    /**
+     * public int[] countersRep()
+     * <p>
+     * Return a counters array, where the value of the i-th entry is the number of trees of order i in the heap.
+     */
+    public int[] countersRep() {
+        int[] arr = new int[42];
+        return arr; //	 to be replaced by student code
+    }
+
+    /**
+     * public void delete(HeapNode x)
+     * <p>
+     * Deletes the node x from the heap.
+     */
+    public void delete(HeapNode x) {
+        return; // should be replaced by student code
+    }
+
+    /**
+     * public void decreaseKey(HeapNode x, int delta)
+     * <p>
+     * The function decreases the key of the node x by delta. The structure of the heap should be updated
+     * to reflect this chage (for example, the cascading cuts procedure should be applied if needed).
+     */
+    public void decreaseKey(HeapNode x, int delta) {
+        return; // should be replaced by student code
+    }
+
+    /**
+     * public int potential()
+     * <p>
+     * This function returns the current potential of the heap, which is:
+     * Potential = #trees + 2*#marked
+     * The potential equals to the number of trees in the heap plus twice the number of marked nodes in the heap.
+     */
+    public int potential() {
+        return 0; // should be replaced by student code
+    }
+
+    /**
+     * public static int totalLinks()
+     * <p>
+     * This static function returns the total number of link operations made during the run-time of the program.
+     * A link operation is the operation which gets as input two trees of the same rank, and generates a tree of
+     * rank bigger by one, by hanging the tree which has larger value in its root on the tree which has smaller value
+     * in its root.
+     */
+    public static int totalLinks() {
+        return 0; // should be replaced by student code
+    }
+
+    /**
+     * public static int totalCuts()
+     * <p>
+     * This static function returns the total number of cut operations made during the run-time of the program.
+     * A cut operation is the operation which diconnects a subtree from its parent (during decreaseKey/delete methods).
+     */
+    public static int totalCuts() {
+        return 0; // should be replaced by student code
+    }
+
+    /**
+     * public class HeapNode
+     * <p>
+     * If you wish to implement classes other than FibonacciHeap
+     * (for example HeapNode), do it in this file, not in
+     * another file
+     */
+    public class HeapNode {
+        public HeapNode(int key) {
+        }
+    }
+}
